abf8d48b2be9aa2f65a3753359a2da137f5cd5a7
Jesus Imery <yisas_0929@hotmail.com>
2019-05-11 10:39:30 -0400

    #2 Gliding bird

---
+++ Assets/Scripts/Character/BirdCharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdCharacterController : MonoBehaviour {
+
+    [SerializeField]
+    float glidingSpeed;
+
+    Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.forward * glidingSpeed;
+        rb.velocity = new Vector3(rb.velocity.x, rb.mass * Physics.gravity.y, rb.velocity.z);
+    }
+}
