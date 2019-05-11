6e7c09820e432a93c8c92829a4c16e6e5d8d7118
Jesus Imery <yisas_0929@hotmail.com>
2019-05-11 18:34:46 -0400

    Revert "#8 Camera tilt when banking"
    This reverts commit 741a8b3be45a585f112322cd30869489645eb10e.

--- Assets/_Temp/Scripts/FollowCamera.cs
+++ Assets/_Temp/Scripts/FollowCamera.cs
@@ -7,7 +7,6 @@ public class FollowCamera : MonoBehaviour
     public GameObject target;
     public float verticalDamping = 1;
     public float horizonalDamping = 1;
-    public float tiltDamping = 1;
     Vector3 offset;
 
     void Start()
@@ -21,19 +20,13 @@ public class FollowCamera : MonoBehaviour
         float desiredAngley = target.transform.eulerAngles.y;
         float currentAnglex = transform.eulerAngles.x;
         float desiredAnglex = target.transform.eulerAngles.x;
-        float currentAnglez = transform.eulerAngles.z;
-        float desiredAnglez = target.transform.eulerAngles.z;
 
         float angley = Mathf.LerpAngle(currentAngley, desiredAngley, Time.deltaTime * verticalDamping);
         //float anglex = Mathf.LerpAngle(currentAnglex, desiredAnglex, Time.deltaTime * horizonalDamping);
-        float anglez = Mathf.LerpAngle(currentAnglez, desiredAnglez, Time.deltaTime * tiltDamping);
          
         Quaternion rotation = Quaternion.Euler(0, angley, 0);
         transform.position = target.transform.position - (rotation * offset);
-        
 
         transform.LookAt(target.transform);
-
-        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, anglez);
     }
 }
