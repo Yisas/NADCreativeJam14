0d9e94dbfb35d36026eaf668c90cebc15898733c
Etienne's Master <salut@courriel.com>
2019-05-12 12:44:12 -0400

    Add story panels

---
+++ Assets/Scripts/StoryManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StoryManager : MonoBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] Sprite[] panels;
+
+    int panelIndex = 0;
+
+    void Start()
+    {
+        image.sprite = panels[panelIndex];
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (panelIndex == panels.Length - 1)
+                SceneManager.LoadScene(2);
+            else
+                image.sprite = panels[++panelIndex];
+        }
+    }
+}
