bc341bef63a34b7f100c96919e4875a4e189d69c
Etienne's Master <salut@courriel.com>
2019-05-12 13:27:56 -0400

    Add cage transition

---
+++ Assets/CameraTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CameraTransition : MonoBehaviour
+{
+    [SerializeField] Animator transition;
+    void Start()
+    {
+        StartCoroutine(Transition());
+    }
+
+    IEnumerator Transition()
+    {
+        transition.CrossFadeInFixedTime("Transparent", 1);
+        yield return new WaitForSeconds(3);
+        GetComponent<Animator>().CrossFadeInFixedTime("CameraNear", 3);
+        transition.CrossFadeInFixedTime("Opaque", 3);
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(3);
+    }
+}
